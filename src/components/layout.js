1b2dc7586adde0a85fec8d87c3cc3f806fd673da
Eric Frommelt <ericfrommelt@gmail.com>
2019-01-01 17:49:06 -0500

    setup react-helmet, wired up webfonts, edited base style

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,11 @@
 import React from "react"
+import {Helmet} from "react-helmet"
 
 export default({ children }) => (
   <div style={{ margin: `0 auto`, maxWidth: 650, padding: `0 1rem`}}>
+    <Helmet>
+      <link rel="stylesheet" type="text/css" href="https://cloud.typography.com/7804816/6709192/css/fonts.css" />
+    </Helmet>
     <h1>Alphanumeric</h1>
     {children}
   </div>
