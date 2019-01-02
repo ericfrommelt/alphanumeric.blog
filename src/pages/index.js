d08423c7624c0c71b18ab90a85f51cad7c577316
Eric Frommelt <ericfrommelt@gmail.com>
2019-01-02 16:09:54 -0500

    added a list of markdown files to index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,39 @@
 import React from "react"
+import { graphql } from "gatsby"
 import "../styles/base.css"
 import Layout from "../components/layout"
 
-export default () => (
-  <Layout>
-    <div>Hello world!</div>
-  </Layout>
-)
+export default ({ data }) => {
+  console.log(data)
+  return (
+    <Layout>
+      <div>
+        <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
+        {data.allMarkdownRemark.edges.map(({ node }) => (
+          <div key={node.id}>
+            <h3>{node.frontmatter.title}{" "} - {node.frontmatter.date}</h3>
+            <p>{node.excerpt}</p>
+          </div>
+        ))}
+      </div>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query {
+    allMarkdownRemark(sort: {fields: [frontmatter___date], order: DESC}) {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD MM, YYYY")
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
